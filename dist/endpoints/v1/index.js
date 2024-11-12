14c9e9ec463e6800aa48be7d941fe0763360ea61
Pixoll <pixoll.gamer@gmail.com>
2024-11-12 14:51:31 -0300

    Ignore dist folder

--- dist/endpoints/v1/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PingEndpoint = void 0;
-var ping_1 = require("./ping");
-Object.defineProperty(exports, "PingEndpoint", { enumerable: true, get: function () { return ping_1.PingEndpoint; } });
-//# sourceMappingURL=index.js.map
