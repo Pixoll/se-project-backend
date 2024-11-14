b0e5b461bfd7e5f688f5053d3c42c610a795b262
Pixoll <pixoll.gamer@gmail.com>
2024-11-13 21:44:45 -0300

    Create PATCH /clinic

--- src/endpoints/v1/clinic.ts
+++ src/endpoints/v1/clinic.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { Clinic, db } from "../../db";
+import { TokenType } from "../../tokens";
 import { SnakeToCamelRecord } from "../../types";
-import { Endpoint, GetMethod } from "../base";
+import { Endpoint, GetMethod, HTTPStatus, PatchMethod } from "../base";
 
 export class ClinicEndpoint extends Endpoint {
     public constructor() {
@@ -9,7 +10,7 @@ export class ClinicEndpoint extends Endpoint {
     }
 
     @GetMethod()
-    public async getClinic(_request: Request, response: Response<ClinicResponse>): Promise<void> {
+    public async getClinic(_request: Request, response: Response<ClinicObject>): Promise<void> {
         const clinic = await db
             .selectFrom("clinic")
             .select([
@@ -28,6 +29,69 @@ export class ClinicEndpoint extends Endpoint {
 
         this.sendOk(response, clinic);
     }
+
+    @PatchMethod({ requiresAuthorization: TokenType.ADMIN })
+    public async updateClinic(request: Request<unknown, unknown, Partial<ClinicObject>>, response: Response): Promise<void> {
+        const {
+            name,
+            email,
+            phone,
+            address,
+            openingTime,
+            closingTime,
+        } = request.body;
+
+        if (!name && !email && !phone && !address && !openingTime && !closingTime) {
+            this.sendError(response, HTTPStatus.BAD_REQUEST, "Request body is empty.");
+            return;
+        }
+
+        const clinic = await db
+            .selectFrom("clinic")
+            .select("id")
+            .executeTakeFirst();
+
+        if (!clinic) {
+            throw new Error("Clinic not found.");
+        }
+
+        let update = db
+            .updateTable("clinic")
+            .where("id", "=", clinic.id);
+
+        if (name) {
+            update = update.set("name", name);
+        }
+
+        if (email) {
+            update = update.set("email", email);
+        }
+
+        if (phone) {
+            update = update.set("phone", phone);
+        }
+
+        if (address) {
+            update = update.set("address", address);
+        }
+
+        if (openingTime) {
+            update = update.set("opening_time", openingTime);
+        }
+
+        if (closingTime) {
+            update = update.set("closing_time", closingTime);
+        }
+
+        const modified = await update.execute();
+
+        if (modified[0].numChangedRows === 0n) {
+            this.sendStatus(response, HTTPStatus.NOT_MODIFIED);
+            return;
+        }
+
+        this.sendStatus(response, HTTPStatus.NO_CONTENT);
+    }
 }
 
-type ClinicResponse = SnakeToCamelRecord<Omit<Clinic, "id">>;
+type ClinicObject = SnakeToCamelRecord<Omit<Clinic, "id">>;
