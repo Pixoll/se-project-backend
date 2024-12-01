7f051d22a572430d217054c8f0fe5f1c3eae614c
Pixoll <pixoll.gamer@gmail.com>
2024-12-01 19:41:14 -0300

    Add colors to logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,17 +1,29 @@
+enum LogType {
+    INFO = "INFO",
+    WARN = "WARN",
+    ERROR = "ERROR",
+}
+
+const logColors = {
+    [LogType.INFO]: "",
+    [LogType.WARN]: "\x1b[33m",
+    [LogType.ERROR]: "\x1b[31m",
+} as const satisfies Record<LogType, string>;
+
 // noinspection JSUnusedGlobalSymbols
 export default {
     log(...messages: unknown[]): void {
-        console.log(prefix("INFO"), ...messages);
+        console.log(prefix(LogType.INFO), ...messages, "\x1b[0m");
     },
     warn(...messages: unknown[]): void {
-        console.warn(prefix("WARN"), ...messages);
+        console.warn(prefix(LogType.WARN), ...messages, "\x1b[0m");
     },
     error(...messages: unknown[]): void {
-        console.error(prefix("ERROR"), ...messages);
+        console.error(prefix(LogType.ERROR), ...messages, "\x1b[0m");
     },
 };
 
-function prefix(logType: "INFO" | "WARN" | "ERROR"): string {
+function prefix(logType: LogType): string {
     const now = new Date().toISOString().replace(/T|Z$/g, " ").trim();
-    return `[${now}] [API] [${logType}]`;
+    return `${logColors[logType]}[${now}] [API] [${logType}]`;
 }
