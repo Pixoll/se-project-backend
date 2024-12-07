9843d3bbac993e1362b7d3bb2cdde7c532b6d55e
Pixoll <pixoll.gamer@gmail.com>
2024-12-06 21:16:15 -0300

    Update appointments.ts

--- src/endpoints/v1/appointments.ts
+++ src/endpoints/v1/appointments.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { sql } from "kysely";
-import { BigIntString, db, TimeSlot } from "../../db";
+import { BigIntString, db } from "../../db";
 import { TokenType } from "../../tokens";
 import { Endpoint, GetMethod } from "../base";
 
@@ -15,12 +15,29 @@ export class AppointmentsEndpoint extends Endpoint {
             .selectFrom("appointment as a")
             .innerJoin("time_slot as t", "t.id", "a.time_slot_id")
             .innerJoin("medic as m", "m.schedule_id", "t.schedule_id")
-            .select([
+            .innerJoin("employee as e", "e.rut", "m.rut")
+            .innerJoin("patient as p", "p.rut", "a.patient_rut")
+            .select(({ ref }) => [
                 "a.id",
                 "m.rut as medicRut",
+                sql<string>`concat(
+                    ${ref("e.first_name")}, " ",
+                    ifnull(concat(${ref("e.second_name")}, " "), ""),
+                    ${ref("e.first_last_name")},
+                    ifnull(concat(" ", ${ref("e.second_last_name")}), "")
+                )`.as("medicFullName"),
                 "a.patient_rut as patientRut",
+                sql<string>`concat(
+                    ${ref("p.first_name")}, " ",
+                    ifnull(concat(${ref("p.second_name")}, " "), ""),
+                    ${ref("p.first_last_name")},
+                    ifnull(concat(" ", ${ref("p.second_last_name")}), "")
+                )`.as("patientFullName"),
+                "p.birth_date as patientBirthDate",
+                "p.email as patientEmail",
+                "p.phone as patientPhone",
                 "a.date",
-                "t.day",
+                "t.id as slotId",
                 "t.start",
                 "t.end",
                 "a.description",
@@ -39,9 +56,14 @@ export class AppointmentsEndpoint extends Endpoint {
 type Appointment = {
     id: BigIntString;
     medicRut: string;
+    medicFullName: string;
     patientRut: string;
+    patientFullName: string;
+    patientBirthDate: string;
+    patientEmail: string;
+    patientPhone: number;
     date: string;
-    day: TimeSlot["day"];
+    slotId: number;
     start: string;
     end: string;
     description: string;
